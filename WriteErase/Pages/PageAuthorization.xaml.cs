4ce293c17c64096ce4e63a20e50a69ad2718111a
КонстантиновАС <@106-10>
2023-03-04 17:59:24 +0300

    Добавьте файлы проекта.

---
+++ WriteErase/Pages/PageAuthorization.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WriteErase.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageAuthorization.xaml
+    /// </summary>
+    public partial class PageAuthorization : Page
+    {
+        public PageAuthorization()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAuthorizate_Click(object sender, RoutedEventArgs e)
+        {
+            var user = Classes.Base.EM.User.FirstOrDefault(x => x.UserLogin == textBoxLogin.Text && x.UserPassword == passBox.Password);
+
+            if (user != null)
+            {
+                MessageBox.Show("Hello");
+                NavigationService.Navigate(new PageProduct());
+            }
+            else
+            {
+                MessageBox.Show("Not Hello");
+            }
+        }
+    }
+}
